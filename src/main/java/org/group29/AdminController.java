047f6f3b304687f74da0bd3fe35ce37d45e01c0b
LivingIron <cveganton@gmail.com>
2021-11-07 22:45:43 +0200

    Made exit button on administrator page functional

---
+++ src/main/java/org/group29/AdminController.java
@@ -0,0 +1,24 @@
+package org.group29;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class AdminController {
+
+    @FXML
+    private Button AdminExitButton;
+
+    public void cancelButtonOnAction(){
+        Stage stage = (Stage) AdminExitButton.getScene().getWindow();
+        stage.close();
+    }
+}
