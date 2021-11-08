7735b68e77777cee2a177d379311ff7965657e0a
LivingIron <cveganton@gmail.com>
2021-11-08 19:49:28 +0200

    Added a new function to search for the Firm ID

--- src/main/java/org/group29/JavaPostgreSQL.java
+++ src/main/java/org/group29/JavaPostgreSQL.java
@@ -68,21 +68,14 @@ public class JavaPostgreSQL {
             if(!res.isBeforeFirst()){
 
                 if(operatorPassword.equals(operatorConfirmPassword)){
-                    res= con.prepareStatement(String.format("SELECT users.id_firm,firm.firm_name,firm.id FROM users FULL JOIN firm ON users.id_firm = firm.id ")).executeQuery();
-                    boolean found=false;
-                    String firmIndex="2";
-                    while (res.next()){
-                        if(res.getString("firm_name").equals(operatorFirm)){
-                            found=true;
-                            firmIndex=res.getString("id");
-                        }
-                    }
-                    if(found == false || operatorFirm .equals("AdminCompany")){
+
+                    String firmIndex= FirmNameToId(operatorFirm);
+
+                    if(firmIndex=="0"){
                         Alert alert = new Alert(Alert.AlertType.ERROR);
                         alert.setContentText("Firm does not exist !");
                         alert.show();
                     }else{
-
                         con.prepareStatement(String.format("INSERT INTO users (username,password,id_firm) VALUES (%s,%s,%s)","'" +operatorUsername +"'","'"+operatorPassword+"'","'"+firmIndex+"'")).executeUpdate();
                     }
 
@@ -104,4 +97,34 @@ public class JavaPostgreSQL {
         }
     }
 
+
+    // Function that takes the firmName , joins users to firm and searches and compares the firmname
+    // If the function succeeds it returns the index of the firm , otherwise it returns 0
+
+    /*-----      Needs to be repurposed to work with all tables when searching for firm id      -----*/
+
+    public static String FirmNameToId(String firmName){
+        String firmIndex="0";
+
+        try(Connection con = DriverManager.getConnection(databaseUrl, databaseUser, databasePassword)){
+
+           ResultSet res= con.prepareStatement(String.format("SELECT users.id_firm,firm.firm_name,firm.id FROM users FULL JOIN firm ON users.id_firm = firm.id ")).executeQuery();
+           boolean found=false;
+
+           while (res.next()){
+                if(res.getString("firm_name").equals(firmName)){
+                    found=true;
+                    firmIndex=res.getString("id");
+                    break;
+                }
+            }
+            if(found == false || firmName .equals("AdminCompany")) firmIndex = "0";
+            return firmIndex;
+        }
+        catch(SQLException ex){
+            Logger lgr=Logger.getLogger(JavaPostgreSQL.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+        return "0";
+    }
 }
