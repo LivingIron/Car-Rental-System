59ca03fc7686dec1a9b6d00b1e7d30bc064ed7e7
LivingIron <cveganton@gmail.com>
2021-11-08 18:04:21 +0200

    Made buttons functional so you can switch between admin operations

--- src/main/java/org/group29/AdminController.java
+++ src/main/java/org/group29/AdminController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -19,8 +20,19 @@ public class AdminController {
     @FXML
     private Button FirmNameAddButton;
     @FXML
+    private Button SwitchToFirm;
+    @FXML
+    private Button SwitchToOperator;
+    @FXML
+    private Button SwitchToVehicle;
+    @FXML
     private TextField FirmNameTextField;
-
+    @FXML
+    private AnchorPane FirmPane;
+    @FXML
+    private AnchorPane OperatorPane;
+    @FXML
+    private AnchorPane VehiclePane;
 
 
     public void cancelButtonOnAction(){
@@ -31,4 +43,32 @@ public class AdminController {
     public void addFirmOnAction(){
         JavaPostgreSQL.AddFirm(FirmNameTextField.getText());
     }
+
+    public  void SwitchToFirmOnAction(){
+        FirmPane.setDisable(false);
+        FirmPane.setVisible(true);
+        OperatorPane.setDisable(true);
+        OperatorPane.setVisible(false);
+        VehiclePane.setDisable(true);
+        VehiclePane.setVisible(false);
+    }
+
+    public  void SwitchToOperatorOnAction(){
+        FirmPane.setDisable(true);
+        FirmPane.setVisible(false);
+        OperatorPane.setDisable(false);
+        OperatorPane.setVisible(true);
+        VehiclePane.setDisable(true);
+        VehiclePane.setVisible(false);
+    }
+
+    public  void SwitchToVehicleOnAction(){
+        FirmPane.setDisable(true);
+        FirmPane.setVisible(false);
+        OperatorPane.setDisable(true);
+        OperatorPane.setVisible(false);
+        VehiclePane.setDisable(false);
+        VehiclePane.setVisible(true);
+    }
+
 }
