36a878e2339e965fe57e4f40294b6babb9d00adf
Alex-SIT <kiruyuu727@gmail.com>
2021-11-18 14:00:03 +0200

    Add Firm class

---
+++ src/main/java/org/group29/entities/Firm.java
@@ -0,0 +1,31 @@
+package org.group29.entities;
+
+public class Firm {
+    private int id;
+    private String name;
+
+    public Firm(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString(){
+        return this.name;
+    }
+}
