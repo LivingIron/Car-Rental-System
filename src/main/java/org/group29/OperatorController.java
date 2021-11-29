9ae629c66c64ff264e8d31fb3a60d4fcd6661f5f
LivingIron <cveganton@gmail.com>
2021-11-29 15:23:04 +0200

    Fixed Car register screen

--- src/main/java/org/group29/OperatorController.java
+++ src/main/java/org/group29/OperatorController.java
@@ -17,30 +17,10 @@ public class OperatorController {
     private Button HomeButton;
     @FXML
     private Button OperatorCloseButton;
-    @FXML
-    private Button AddVehicleButton;
-
-    /*---------------Radio Buttons----------------*/
-    @FXML
-    private RadioButton RadioSmoking;
-    @FXML
-    private RadioButton RadioNonSmoking;
-
-    /*---------------ComboBoxes----------------*/
-    @FXML
-    private ComboBox<VehicleClass> ClassComboBox;
-    @FXML
-    private ComboBox<VehicleCategory> CategoryComboBox;
 
     /*---------------PANES----------------*/
     @FXML
     private BorderPane mainPane;
-
-
-
-    /*---------------Text Areas ----------------*/
-    @FXML
-    private TextArea VehicleTextArea;
 
     /*--------------------Labels----------------*/
     @FXML
@@ -56,7 +36,6 @@ public class OperatorController {
 
     }
 
-
     /*--------------------------------------Functions for buttons-------------------------------------------*/
 
     public void exitSceneOnAction(){
@@ -64,49 +43,4 @@ public class OperatorController {
         stage.close();
     }
 
-    public void addVehicleOnAction(){
-        if(ClassComboBox.getValue()==null){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("Please select a Class!");
-            alert.show();
-            return;
-        }
-        if(CategoryComboBox.getValue()==null){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("Please select a Category!");
-            alert.show();
-            return;
-        }
-        if(VehicleTextArea.getText().equals("")){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("Please fill out the vehicle characteristics!");
-            alert.show();
-            return;
-        }
-        if(!RadioSmoking.isSelected() && !RadioNonSmoking.isSelected()){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("Please select if the vehicle is for smokers or not!");
-            alert.show();
-            return;
-        }
-        boolean isForSmokers = RadioSmoking.isSelected();
-
-        JavaPostgreSQL.addVehicle(ClassComboBox.getValue().getId(),
-                CategoryComboBox.getValue().getId(),
-                Data.operatorId,
-                VehicleTextArea.getText(),
-                isForSmokers);
-    }
-
-    /*-------------------------------Functions for populating ComboBoxes-----------------------------------*/
-
-    private void populateClassComboBox(){
-        VehicleClass[] strings = JavaPostgreSQL.getClassNames();
-        ClassComboBox.setItems(FXCollections.observableArrayList(strings));
-    }
-
-    private void populateCategoryComboBox(){
-        VehicleCategory[] strings = JavaPostgreSQL.getCategoryNames();
-        CategoryComboBox.setItems(FXCollections.observableArrayList(strings));
-    }
 }
---
+++ src/main/java/org/group29/RegisterCarController.java
@@ -0,0 +1,85 @@
+package org.group29;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import org.group29.entities.VehicleCategory;
+import org.group29.entities.VehicleClass;
+
+import java.net.URL;
+
+public class RegisterCarController {
+
+    /*---------------Radio Buttons----------------*/
+    @FXML
+    private RadioButton RadioSmoking;
+    @FXML
+    private RadioButton RadioNonSmoking;
+
+    /*---------------ComboBoxes----------------*/
+    @FXML
+    private ComboBox<VehicleClass> ClassComboBox;
+    @FXML
+    private ComboBox<VehicleCategory> CategoryComboBox;
+
+    /*---------------Text Areas ----------------*/
+    @FXML
+    private TextArea VehicleTextArea;
+
+
+    @FXML
+    protected void initialize() {
+        populateClassComboBox();
+        populateCategoryComboBox();
+    }
+
+    public void addVehicleOnAction(){
+        if(ClassComboBox.getValue()==null){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Please select a Class!");
+            alert.show();
+            return;
+        }
+        if(CategoryComboBox.getValue()==null){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Please select a Category!");
+            alert.show();
+            return;
+        }
+        if(VehicleTextArea.getText().equals("")){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Please fill out the vehicle characteristics!");
+            alert.show();
+            return;
+        }
+        if(!RadioSmoking.isSelected() && !RadioNonSmoking.isSelected()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Please select if the vehicle is for smokers or not!");
+            alert.show();
+            return;
+        }
+        boolean isForSmokers = RadioSmoking.isSelected();
+
+        JavaPostgreSQL.addVehicle(ClassComboBox.getValue().getId(),
+                CategoryComboBox.getValue().getId(),
+                Data.operatorId,
+                VehicleTextArea.getText(),
+                isForSmokers);
+    }
+
+    /*-------------------------------Functions for populating ComboBoxes-----------------------------------*/
+
+    private void populateClassComboBox(){
+        VehicleClass[] strings = JavaPostgreSQL.getClassNames();
+        ClassComboBox.setItems(FXCollections.observableArrayList(strings));
+    }
+
+    private void populateCategoryComboBox(){
+        VehicleCategory[] strings = JavaPostgreSQL.getCategoryNames();
+        CategoryComboBox.setItems(FXCollections.observableArrayList(strings));
+    }
+
+}
