1c5e8f2bc3136ae5dcf0b453dd45b4d23039351a
Alex-SIT <kiruyuu727@gmail.com>
2021-12-11 16:15:07 +0200

    Add CheckClientRatings functionality

--- src/main/java/org/group29/CheckClientRatingsController.java
+++ src/main/java/org/group29/CheckClientRatingsController.java
@@ -1,4 +1,27 @@
 package org.group29;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.group29.entities.Client;
+
 public class CheckClientRatingsController {
+    @FXML
+    private TableView<Client> ratingsTable;
+    @FXML
+    private TableColumn<Client, String> clientColumn;
+    @FXML
+    private TableColumn<Client, String> ratingColumn;
+
+    @FXML
+    private void initialize(){
+        Client[] clients = JavaPostgreSQL.getClients();
+
+        clientColumn.setCellValueFactory(p -> new ReadOnlyObjectWrapper<>(p.getValue().getName()));
+        ratingColumn.setCellValueFactory(p -> new ReadOnlyObjectWrapper<>(Integer.toString(p.getValue().getRating())));
+
+        ratingsTable.setItems(FXCollections.observableArrayList(clients));
+    }
 }
