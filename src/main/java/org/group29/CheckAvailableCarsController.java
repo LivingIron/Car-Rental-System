0914efd8621a8ca30a5bfa49730780b0a3e1cb41
Alex-SIT <kiruyuu727@gmail.com>
2021-12-24 22:36:29 +0200

    Add "View photos" to context menu in CheckAvailableCars list

--- src/main/java/org/group29/CheckAvailableCarsController.java
+++ src/main/java/org/group29/CheckAvailableCarsController.java
@@ -1,14 +1,19 @@
 package org.group29;
 
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.ListView;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.group29.entities.Vehicle;
+import org.group29.entities.VehiclePhoto;
 
-import java.time.LocalDate;
 import java.sql.Date;
+import java.time.LocalDate;
 
 public class CheckAvailableCarsController {
 
@@ -44,6 +49,57 @@ public class CheckAvailableCarsController {
         Date endDate = Date.valueOf(endDatePicker.getValue());
 
         Vehicle[] cars = JavaPostgreSQL.getAvailableVehicles(startDate, endDate);
+
         vehicleList.setItems(FXCollections.observableArrayList(cars));
+        vehicleList.setCellFactory(lv -> {
+            ListCell<Vehicle> cell = new ListCell<>();
+
+            ContextMenu contextMenu = new ContextMenu();
+
+            MenuItem viewItem = new MenuItem();
+            viewItem.textProperty().bind(Bindings.format("View photos"));
+            viewItem.setOnAction(event -> {
+                Vehicle item = cell.getItem();
+                VehiclePhoto[] photos = JavaPostgreSQL.getPhotosByVehicle(item);
+                if(photos.length == 0){
+                    Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+                    newAlert.setContentText("Selected vehicle has no photos");
+                    newAlert.show();
+                }
+                else createGalleryWindow(photos);
+            });
+            contextMenu.getItems().add(viewItem);
+
+            cell.emptyProperty().addListener((obs, wasEmpty, isNowEmpty) -> {
+                if (isNowEmpty) {
+                    cell.setContextMenu(null);
+                } else {
+                    cell.textProperty().bind(cell.itemProperty().asString());
+                    cell.setContextMenu(contextMenu);
+                }
+            });
+            return cell;
+        });
+    }
+
+    private void createGalleryWindow(VehiclePhoto[] photos){
+        Stage stage = new Stage();
+        FlowPane flowPane = new FlowPane();
+        for(VehiclePhoto photo : photos) {
+            flowPane.getChildren().add(new ImageView(photo.toImage(0, 420, true)));
+        }
+        ScrollPane scroll = new ScrollPane(flowPane);
+        scroll.setPrefWidth(1280);
+        scroll.setPrefHeight(720);
+        scroll.fitToWidthProperty().set(true);
+        Scene scene = new Scene(scroll);
+
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Photo gallery");
+        stage.initStyle(StageStyle.UTILITY);
+        stage.initOwner(vehicleList.getScene().getWindow());
+
+        stage.show();
     }
 }
