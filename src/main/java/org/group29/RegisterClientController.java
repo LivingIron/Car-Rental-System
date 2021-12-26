f76f0459baf2033d6c6ed15bee3b1ff9fb1b774d
Alex-SIT <kiruyuu727@gmail.com>
2021-12-26 04:09:38 +0200

    Add check for phone number length

--- src/main/java/org/group29/RegisterClientController.java
+++ src/main/java/org/group29/RegisterClientController.java
@@ -13,30 +13,35 @@ public class RegisterClientController {
     private TextField clientPhoneTextField;
 
     public void addClientOnAction(){
-        if(clientPhoneTextField.getText().equals("")||clientNameTextField.getText().equals("")){
+        if(clientPhoneTextField.getText().equals("") || clientNameTextField.getText().equals("")){
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setContentText("Please fill all the empty fields!");
             alert.show();
+            return;
+        }
+        if(clientPhoneTextField.getLength() != 10){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Invalid phone number!");
+            alert.show();
+            return;
+        }
+        Client newClient = new Client(-1,
+                Data.operator.getFirm_id(),
+                5,
+                clientNameTextField.getText(),
+                clientPhoneTextField.getText());
+
+        newClient.commit();
+
+        Alert alert;
+        if(newClient.getId() != -1){
+            alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("New client registered!");
         }
         else{
-            Client newClient = new Client(-1,
-                    Data.operator.getFirm_id(),
-                    5,
-                    clientNameTextField.getText(),
-                    clientPhoneTextField.getText());
-
-            newClient.commit();
-
-            Alert alert;
-            if(newClient.getId() != -1){
-                alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setContentText("New client registered!");
-            }
-            else{
-                alert = new Alert(Alert.AlertType.ERROR);
-                alert.setContentText("Client already exists!");
-            }
-            alert.show();
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Client already exists!");
         }
+        alert.show();
     }
 }
